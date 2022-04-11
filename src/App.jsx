c3a93d1be04ba285b4e66350300d2e808713e2ff
GameGodS3 <sudevssuresh@gmail.com>
2022-04-11 22:24:17 +0530

    Merged addition of routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
-import './App.css'
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import CameraScreen from './pages/CameraScreen.jsx'
-import ReceptionistScreen from './pages/ReceptionistScreen.jsx'
-import UserSelectionScreen from './pages/UserSelectionScreen.jsx'
-import PharmacyViewScreen from './pages/PharmacyViewScreen.jsx'
-import PatientHistoryScreen from './pages/PatientHistoryScreen.jsx'
-import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
+import CameraScreen from "./pages/CameraScreen.jsx";
+// import ReceptionistScreen from './pages/ReceptionistScreen.jsx'
+import UserSelectionScreen from "./pages/UserSelectionScreen.jsx";
+import Doctor from "./pages/Doctor";
+
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
 
 function App() {
-
   return (
-    <div className='App'>
-      <Router >
+    <div className="App">
+      <Router>
         <Routes>
           <Route path="/" element={<UserSelectionScreen />} />
           <Route path="/patient" element={<div />} />
-          <Route path="/camera" element={<CameraScreen/>} />
-          <Route path="/doctor" element={<PatientHistoryScreen/>} />
-          <Route path="/receptionist" element={<ReceptionistScreen/>} />
-          <Route path="/pharmacy" element={<PharmacyViewScreen/>} />
+          <Route path="/camera" element={<CameraScreen />} />
+          <Route path="/doctor" element={<Doctor />} />
+
+          {/*<Route path="/receptionist" element={<ReceptionistScreen/>} />*/}
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
